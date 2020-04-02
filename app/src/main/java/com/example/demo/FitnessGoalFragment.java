8e59aaf1fa4a42e13c9ad25be3679dd5976cc6f8
yaweiz <yawei19970710@outlook.com>
2020-04-02 00:09:56 -0400

    Working on meal interface

--- app/src/main/java/com/example/demo/FitnessGoalFragment.java
+++ app/src/main/java/com/example/demo/FitnessGoalFragment.java
@@ -25,7 +25,9 @@ public class FitnessGoalFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fitness_goal, container, false);
+//        return inflater.inflate(R.layout.fragment_fitness_goal, container, false);
+        return inflater.inflate(R.layout.activity_me, container, false);
+
     }
 
 }
--- app/src/main/java/com/example/demo/LoginActivity.java
+++ app/src/main/java/com/example/demo/LoginActivity.java
@@ -39,7 +39,8 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+//        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_login_decorated);
 
         mAuth = FirebaseAuth.getInstance();
 
