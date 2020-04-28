748c4ae37b2e042460f4864983984dd1f1e256ee
yaweiz <yawei19970710@outlook.com>
2020-04-28 00:27:50 -0400

    Change step UI and homepage UI

--- app/src/main/java/com/example/demo/DailyStepFragment.java
+++ app/src/main/java/com/example/demo/DailyStepFragment.java
@@ -8,6 +8,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.util.Random;
 
 
 /**
@@ -15,6 +19,8 @@ import android.view.ViewGroup;
  */
 public class DailyStepFragment extends Fragment {
 
+    TextView step;
+    TextView step_progress;
 
     public DailyStepFragment() {
         // Required empty public constructor
@@ -25,7 +31,27 @@ public class DailyStepFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_daily_step, container, false);
+//        return inflater.inflate(R.layout.fragment_daily_step, container, false);
+        View v = inflater.inflate(R.layout.fragment_daily_step, container, false);
+        step = v.findViewById(R.id.textView2);
+        step_progress = v.findViewById(R.id.step_progress);
+        Random random = new Random();
+        int maxV = 5012;
+        int minV = 5011;
+        int stepCount = random.nextInt(maxV) % (maxV - minV + 1) + minV;
+        step.setText("Walk " + stepCount + " steps");
+        step_progress.setText(String.valueOf(stepCount));
+
+        ProgressBar progress = v.findViewById(R.id.circle_progress_bar);
+        progress.setProgress(62);
+
+        return v;
     }
+
+
+//    public void setText(String text){
+//        step.setText("000");
+//    }
+
 
 }
--- app/src/main/java/com/example/demo/FitnessGoalFragment.java
+++ app/src/main/java/com/example/demo/FitnessGoalFragment.java
@@ -3,11 +3,18 @@ package com.example.demo;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 
 /**
@@ -15,6 +22,14 @@ import android.view.ViewGroup;
  */
 public class FitnessGoalFragment extends Fragment {
 
+    private FirebaseAuth mAuth;
+
+    private View me_dailyStep;
+//    private View home_mealplan;
+//    private View home_fitnessgoal;
+    private NavController navController;
+
+    TextView userName;
 
     public FitnessGoalFragment() {
         // Required empty public constructor
@@ -25,9 +40,24 @@ public class FitnessGoalFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-//        return inflater.inflate(R.layout.fragment_fitness_goal, container, false);
-        return inflater.inflate(R.layout.activity_me, container, false);
+        return inflater.inflate(R.layout.fragment_fitness_goal, container, false);
+//        return inflater.inflate(R.layout.activity_me, container, false);
 
+    }
+
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState){
+        super.onActivityCreated(savedInstanceState);
+        FragmentActivity activity = requireActivity();
+
+        me_dailyStep = activity.findViewById(R.id.me_item_reord);
+        me_dailyStep.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(v).navigate(R.id.dailyStepFragment);
+            }
+        });
     }
 
 }
